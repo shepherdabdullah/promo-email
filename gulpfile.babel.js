20a9c3a5cfb431b86dcdc50bfbee4c545c3a8eed
abdullah <shepardabdullah@hotmail.com>
2022-10-17 10:24:50 -0500

    create json for offers data

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -58,6 +58,7 @@ function pages() {
       root: 'src/pages',
       layouts: 'src/layouts',
       partials: 'src/partials',
+      data: 'src/data',
       helpers: 'src/helpers'
     }))
     .pipe(inky())
